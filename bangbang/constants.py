d43effc84df6264dd9f0811799e98f4d73440cf6
nonoesimposible <nonoesimposible72@gmail.com>
2023-07-21 22:57:39 -0600

    first stage of network refactor
    also includes lots of code cleanup and the start of dropping Python 2
    support because I didn't have the self-control to wait until later to
    address the sadness

---
+++ bangbang/constants.py
@@ -0,0 +1,30 @@
+import enum
+
+PORT = 4320
+SERVER_START_KEYWORD = "start"
+VERSION = "1.3.0a"
+
+
+# raises ValueError if message types share a duplicate value
+@enum.unique
+class Msg(enum.IntEnum):
+    """Enum for message types."""
+
+    APPROVE = enum.auto()  # server broadcasts approval to a REQUEST
+    GREET = enum.auto()    # client informs server of name, maybe color, etc.
+    ID = enum.auto()       # server informs client of assigned id
+    REQUEST = enum.auto()  # client requests server to move, shoot, etc.
+    START = enum.auto()    # game starts
+
+
+@enum.unique
+class Rq(enum.IntEnum):
+    """
+    Enum for request types.
+
+    A request is a message sent by a player asking to move, shoot, etc.
+    """
+
+    UP = enum.auto()
+    DOWN = enum.auto()
+    SHOOT = enum.auto()
