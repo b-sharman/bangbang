b-sharman/bangbang

commit b5a50fb1f5c6c03591da2089425260fad270215d

optimize network traffic (partial regressions)
Instead of sending data every 50 ms, data is sent every time a set of
tank actions changes.

bangbang/game.py
@@ -19,7 +19,7 @@ from OpenGL.GLU import *
 
 import utils_3d
 import bbutils
-from client import Client, PlayerData
+import client
 import collisions
 import constants
 import shapes
@@ -34,7 +34,7 @@ pygame.init()
 
 class Game:
     def __init__(self, no_music) -> None:
-        self.client = Client(self)
+        self.client = client.Client(self)
 
         # used to block opening the window until the game has started
         self.start_event = asyncio.Event()
@@ -43,7 +43,7 @@ class Game:
         # assigned upon receiving an ID message
         self.player_id = None
 
-        self.players: dict[int, PlayerData] = {}
+        self.initial_states: list[tuple[int, dict]] = []
 
         # set width when client receives START from server
         self.ground_hw: int | None = None
@@ -98,28 +98,28 @@ class Game:
                     shape.hill()
 
                 # remove shells colliding with tanks
-                for tank in self.groups.tanks:
-                    if collisions.collide_tank(shape.pos, tank.state.pos, tank.state.bout):
+                for tank in self.groups.tanks.values():
+                    if collisions.collide_tank(shape.pos, tank.pos, tank.bout):
                         shape.die()
 
         # tank-tree collisions
         for tree in filter(lambda t: not t.is_falling, self.groups.trees):
-            for player_data in self.players.values():
-                if collisions.collide_tank(player_data.pos, tree.pos, player_data.bout):
-                    tree.fall(player_data.bright, player_data.speed)
+            for tank in self.groups.tanks.values():
+                if collisions.collide_tank(tank.pos, tree.pos, tank.bout):
+                    tree.fall(tank.bright, tank.speed)
 
     async def handle_message(self, message: dict) -> None:
         """Handle a JSON-loaded dict network message."""
         match message["type"]:
             case constants.Msg.APPROVE:
                 try:
-                    self.players[message["id"]].update_state(message["state"])
+                    self.groups.tanks[message["id"]].update_state(message["state"])
                 except KeyError:
                     logging.log(logging.DEBUG, f"received APPROVE for player {message['id']} which does not exist")
                 else:
                     pass
                     # logging.debug(
-                    #     f"PlayerData {message['id']} state updated with {message['state']}"
+                    #     f"Tank {message['id']} state updated with {message['state']}"
                     # )
 
             case constants.Msg.ID:
@@ -131,7 +131,7 @@ class Game:
                         self,
                         message["id"],
                         message["pos"],
-                        self.players[message["id"]].color
+                        self.groups.tanks[message["id"]].color
                     )
                 )
 
@@ -143,14 +143,11 @@ class Game:
 
             case constants.Msg.START:
                 logging.debug("starting")
-                for client_id, state in message["states"]:
-                    self.players[client_id] = PlayerData(state)
+                self.initial_states += message["states"]
 
                 self.ground_hw = message["ground_hw"]
                 self.hill_poses = message["hill_poses"]
                 self.tree_poses = message["tree_poses"]
-                for client_id, state in message["states"]:
-                    self.players[client_id].update_state(state)
 
                 # allow the main game loop to start
                 self.start_event.set()
@@ -206,21 +203,20 @@ class Game:
         # set up the explosion displaylists
         shapes.setup_explosion()
 
-        # NATURE
-
         # make the sky blue
         glClearColor(0.25, 0.89, 0.92, 1.0)
-
-        ground = shapes.Ground(self.ground_hw)
-        self.lifebar = shapes.LifeBar(self.players[self.player_id], SCR)
-        self.reloadingbar = shapes.ReloadingBar(SCR[0])
 
         # TODO: no special group is needed for hills
         self.groups.hills = [shapes.Hill(pos) for pos in self.hill_poses]
         self.groups.trees = [shapes.Tree(pos) for pos in self.tree_poses]
-        self.groups.tanks = [shapes.Tank(self, client_id) for client_id in self.players]
+        self.groups.tanks = {client_id: shapes.Tank(self, client_id, state) for client_id, state in self.initial_states}
 
-        self.groups.all_shapes = [ground] + self.groups.hills + self.groups.trees + self.groups.tanks
+        # single-instance shapes
+        ground = shapes.Ground(self.ground_hw)
+        self.lifebar = shapes.LifeBar(self.groups.tanks[self.player_id], SCR)
+        self.reloadingbar = shapes.ReloadingBar(SCR[0])
+
+        self.groups.all_shapes = [ground] + self.groups.hills + self.groups.trees + list(self.groups.tanks.values())
 
         # start listening for keyboard input
         # self.tg is defined in initialize
@@ -270,10 +266,10 @@ class Game:
                 pos = self.spectator.pos
                 out = self.spectator.out
             else:
-                pos = np.array(self.player_data.pos)
+                pos = np.array(self.this_player.pos)
                 # TODO: define a constant for this magic number
                 pos[1] = 6.0
-                out = np.array(self.player_data.tout)
+                out = np.array(self.this_player.tout)
                 at = out + pos
             gluLookAt(*pos, *at, *constants.UP)
 
@@ -288,7 +284,11 @@ class Game:
             # I think this is a little slower than list.remove() because a whole new
             # linked list has to be built; however, it seems more Pythonic than
             # looping through and calling remove()
-            self.groups.tanks = [tank for tank in self.groups.tanks if tank.alive]
+            self.groups.tanks = {
+                client_id: tank
+                for client_id, tank in self.groups.tanks.items()
+                if tank.alive
+            }
             self.groups.all_shapes = [shape for shape in self.groups.all_shapes if shape.alive]
 
             pygame.display.flip()
@@ -300,8 +300,8 @@ class Game:
             frame_length = frame_end_time - frame_start_time
 
     @property
-    def player_data(self) -> PlayerData:
-        return self.players[self.player_id]
+    def this_player(self) -> shapes.Tank:
+        return self.groups.tanks[self.player_id]
 
 
 class PlayerInputHandler:
@@ -314,7 +314,7 @@ class PlayerInputHandler:
         """Interface to send requests to the server."""
         # to avoid sending the same set of actions twice in a row
         prev_actions = None
-        while self.game.player_data.alive:
+        while self.game.this_player.alive:
             try:
                 pressed = pygame.key.get_pressed()
                 actions: list[constants.Action] = [

bangbang/shapes.py
@@ -8,7 +8,6 @@ with contextlib.redirect_stdout(None):
     import pygame
 
 from base_shapes import Shape
-from client import PlayerData
 from collections.abc import Iterable
 import constants
 import utils_3d
@@ -120,6 +119,178 @@ class HeadlessShell(Shape, constants.Shell):
         self.pos += self.out * Shell.SPEED * self.delta_time()
 
 
+class HeadlessTank(Shape, constants.Tank):
+    def __init__(self, angle, client_id, color, ground_hw, name, pos):
+        super().__init__()
+
+        self.bangle = angle  # base angle
+        self.tangle = angle  # turret angle
+        self.client_id = client_id
+        self.color = color
+        self.ground_hw = ground_hw
+        self.name = name
+        self.pos = np.array(pos)
+
+        # snapping back: turret turns to meet base
+        # turning back: base turns to meet turret
+        # these variables are true if either t or ctrl t have been pressed
+        # I believe they can both be true simulatneously
+        self.snapping_back = False
+        self.turning_back = False
+
+        self.hits_left: int = self.HITS_TO_DIE  # how many more hits before dead?
+        self.speed = 0.0  # m / s, I hope
+
+        self.actions: set[constants.Action] = set()
+
+    def recv_hit(self, damage: int) -> None:
+        """Decrement the tank health by damage."""
+        self.hits_left -= damage
+        if self.hits_left <= 0:
+            self.die()
+
+    def snap_logic(self, target_angle, approaching_angle, incr):
+        """
+        Perform calculations for the snapping and turning back animations.
+
+        Returns the increment to be added to the approaching angle (which may be 0)
+        and a boolean indicating whether the animation is still going.
+
+        incr should be delta * (self.SNAP_SPEED or self.BROTATE).
+        """
+        diff = approaching_angle - target_angle
+        # the sign of the increment
+        direction = (diff < 0) - (diff > 0)
+        diff = abs(diff)
+        # always choose the shortest path
+        if diff > 180.0:
+            diff = 360 - diff
+            direction *= -1
+
+        # once within a certain threshold of the correct angle, stop snapping back
+        if diff <= incr:
+            return diff * direction, False
+        return incr * direction, True
+
+    def update(self) -> None:
+        delta = self.delta_time()
+
+        ip_bangle = 0
+        ip_tangle = 0
+
+        # TODO: ask some knowledgeable folks whether a match-case pattern would be more
+        # appropriate here
+        if constants.Action.ACCEL in self.actions:
+            self.speed = min(self.speed + self.ACC * delta, self.MAX_SPEED)
+
+        if constants.Action.ALL_LEFT in self.actions:
+            self.turning_back = False
+            self.snapping_back = False
+            ip_bangle = self.BROTATE
+            ip_tangle = self.BROTATE
+
+        if constants.Action.ALL_RIGHT in self.actions:
+            self.turning_back = False
+            self.snapping_back = False
+            ip_bangle = -self.BROTATE
+            ip_tangle = -self.BROTATE
+
+        if constants.Action.BASE_LEFT in self.actions:
+            # cancel turning back upon manual turn
+            self.turning_back = False
+            ip_bangle = self.BROTATE
+
+        if constants.Action.BASE_RIGHT in self.actions:
+            # cancel turning back upon manual turn
+            self.turning_back = False
+            ip_bangle = -self.BROTATE
+
+        if constants.Action.DEACCEL in self.actions:
+            self.speed = max(self.speed - self.ACC * delta, self.MIN_SPEED)
+
+        if constants.Action.TURN_BACK in self.actions:
+            self.turning_back = True
+
+        if constants.Action.TURRET_LEFT in self.actions:
+            # cancel snapping back upon manual turn
+            self.snapping_back = False
+            ip_tangle = self.TROTATE
+
+        if constants.Action.TURRET_RIGHT in self.actions:
+            # cancel snapping back upon manual turn
+            self.snapping_back = False
+            ip_tangle = -self.TROTATE
+
+        if constants.Action.SNAP_BACK in self.actions:
+            self.snapping_back = True
+
+        if constants.Action.STOP in self.actions and abs(self.speed) <= self.SNAP_STOP:
+            self.speed = 0.0
+
+        # snap_logic returns the number of degrees to turn this frame; all the other
+        # logic specifies the rate of turning in degrees per second
+        if not self.snapping_back:
+            ip_tangle *= delta
+        if not self.turning_back:
+            ip_bangle *= delta
+
+        # handle snapping/turning back
+        if self.snapping_back and ip_tangle == 0.0:
+            ip_tangle, self.snapping_back = self.snap_logic(
+                self.bangle, self.tangle, self.SNAP_SPEED * delta
+            )
+        if self.turning_back and ip_bangle == 0.0:
+            ip_bangle, self.turning_back = self.snap_logic(
+                self.tangle, self.bangle, self.BROTATE * delta
+            )
+
+        # adjust base and turret angles and out vectors if they've changed
+        if ip_bangle:
+            self.bangle += ip_bangle
+        if ip_tangle:
+            self.tangle += ip_tangle
+
+        # make sure the angles don't get too high, this helps the turret animation
+        self.bangle %= 360.0
+        self.tangle %= 360.0
+
+        # move the tank, according to the speed
+        self.pos += self.bout * self.speed * delta
+        # ensure the tank does not go over the edge of the world
+        self.pos[0] = max(min(self.pos[0], self.ground_hw), -self.ground_hw)
+        self.pos[2] = max(min(self.pos[2], self.ground_hw), -self.ground_hw)
+
+    @property
+    def state(self):
+        # TODO: make this dynamically change to not send already-known data
+        return {
+            "bangle": self.bangle,
+            "color": self.color,
+            "hits_left": self.hits_left,
+            "name": self.name,
+            "pos": tuple(self.pos),
+            "speed": self.speed,
+            "tangle": self.tangle,
+        }
+
+    # vector properties
+
+    @property
+    def bout(self):
+        # don't know if this is correct - will need some trial and error
+        return utils_3d.yaw(self.bangle, np.array((0.0, 0.0, 1.0)), np.array((1.0, 0.0, 0.0)))
+
+    @property
+    def tout(self):
+        # don't know if this is correct - will need some trial and error
+        return utils_3d.yaw(self.tangle, np.array((0.0, 0.0, 1.0)), np.array((1.0, 0.0, 0.0)))
+
+    # TODO: if these vectors are actually left instead of right, rename them
+    @property
+    def bright(self):
+        return utils_3d.normalize(np.cross(constants.UP, self.bout))
+
+
 class Hill(Shape, constants.Hill):
     def __init__(self, pos):
         super().__init__()
@@ -151,8 +322,8 @@ class LifeBar(constants.LifeBar):
         pygame.image.load("../data/images/AAGH5.png"),
     )
 
-    def __init__(self, player_data: PlayerData, screen: tuple[int]):
-        self.player_data = player_data
+    def __init__(self, tank: "shapes.Tank", screen: tuple[int]):
+        self.tank = tank
 
         glPushMatrix()
         glLoadIdentity()
@@ -237,7 +408,7 @@ class LifeBar(constants.LifeBar):
         """Draw the LifeBar overlay."""
         glPushMatrix()
         glLoadIdentity()
-        glCallList(self.gllists[self.player_data.hits_left])
+        glCallList(self.gllists[self.tank.hits_left])
         glPopMatrix()
 
 
@@ -432,7 +603,7 @@ class Spectator(Shape, constants.Spectator):
         return utils_3d.normalize(np.cross(constants.UP, self.out))
 
 
-class Tank(Shape):
+class Tank(HeadlessTank):
     # The tank model is composed of two models: the turret and the base. This is so that
     # the turret can spin independently of the base. B stands for base, T stands for
     # turret.
@@ -440,8 +611,15 @@ class Tank(Shape):
     blist = "Unknown"
     tlist = "Unknown"
 
-    def __init__(self, game: "game.Game", client_id: int):
-        super().__init__()
+    def __init__(self, game: "game.Game", client_id: int, state: dict):
+        super().__init__(
+            state["bangle"],
+            client_id,
+            state["color"],
+            game.ground_hw,
+            state["name"],
+            state["pos"],
+        )
 
         if Tank.blist == "Unknown":
             Tank.blist = glGenLists(1)
@@ -456,22 +634,27 @@ class Tank(Shape):
 
         self.game = game
         self.client_id = client_id
+        self.update_state(state)
 
-    def update(self):
+    def update(self) -> None:
+        super().update()
+
         for angle, gllist in (
-            (self.state.bangle, self.blist),
-            (self.state.tangle, self.tlist),
+            (self.bangle, self.blist),
+            (self.tangle, self.tlist),
         ):
             glPushMatrix()
-            glColor(*self.state.color)
-            glTranslate(*self.state.pos)
+            glColor(*self.color)
+            glTranslate(*self.pos)
             glRotate(angle, *constants.UP)
             glCallList(gllist)
             glPopMatrix()
 
-    @property
-    def state(self):
-        return self.game.players[self.client_id]
+    def update_state(self, state: dict) -> None:
+        """Force-rewrite values such as pos, bangle, etc."""
+        if "pos" in state:
+            state["pos"] = np.array(state["pos"])
+        self.__dict__.update(state)
 
 
 class Tree(Shape, constants.Tree):

bangbang/client.py
@@ -10,49 +10,6 @@ import constants
 import utils_3d
 
 
-class PlayerData:
-    """Class that updates its __dict__ from data coming over the network."""
-
-    def __init__(self, state: Optional[dict[str, Any]] = None) -> None:
-        if state is not None:
-            self.update_state(state)
-
-    def update_state(self, state: dict[str, Any]) -> None:
-        """
-        Assign this class's attributes to values corresponding to `state`.
-
-        This essentially translates a dict of vars and values to attributes
-        For instance,
-            >>> p = PlayerData()
-            >>> p.update_state({"client_id": 0, "name": "foo"})
-        should be more or less equivalent to
-            >>> p = PlayerData()
-            >>> p.client_id = 0
-            >>> p.name = "foo"
-        """
-        self.__dict__.update(state)
-
-    @property
-    def alive(self):
-        return self.hits_left > 0
-
-    # TODO: this is duplicated code from server.Tank - find a way to reuse it
-    @property
-    def bout(self):
-        # don't know if this is correct - will need some trial and error
-        return utils_3d.yaw(self.bangle, np.array((0.0, 0.0, 1.0)), np.array((1.0, 0.0, 0.0)))
-
-    @property
-    def tout(self):
-        # don't know if this is correct - will need some trial and error
-        return utils_3d.yaw(self.tangle, np.array((0.0, 0.0, 1.0)), np.array((1.0, 0.0, 0.0)))
-
-    # TODO: if these vectors are actually left instead of right, rename them
-    @property
-    def bright(self):
-        return utils_3d.normalize(np.cross(constants.UP, self.bout))
-
-
 class Client:
     def __init__(self, game: "game.Game") -> None:
         self.game = game

commit afb919e873254a75dd9261b09eabce9877815cce

remove flickering artifacts caused by b5a50fb

bangbang/constants.py
@@ -18,6 +18,9 @@ TREE_BUFFER = 0  # m
 # minimum spawning distance between tanks
 MIN_SPAWN_DIST = 50  # m
 
+# height at which gluLookAt is called
+CAMERA_HEIGHT = 6.0  # m
+
 # network constants
 PORT = 4320
 SERVER_START_KEYWORD = "start"

bangbang/game.py
@@ -257,25 +257,31 @@ class Game:
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
             glLoadIdentity()
 
-            # set up the observer
-            # choose the camera attributes based on whether we're playing or spectating
-            # TODO; How expensive is hasattr? It could be replaced by a
-            # self.is_spectating variable or a class that stores just the pos and out
-            # of either the player or the spectator.
-            if hasattr(self, "spectator"):
-                pos = self.spectator.pos
-                out = self.spectator.out
-            else:
-                pos = np.array(self.this_player.pos)
-                # TODO: define a constant for this magic number
-                pos[1] = 6.0
-                out = np.array(self.this_player.tout)
-                at = out + pos
-            gluLookAt(*pos, *at, *constants.UP)
-
             self.collisions()
 
+            # gluLookAt needs to be called immediately after player update to avoid
+            # "jumping" artifacts resulting from respective .pos 's being updated at
+            # slightly different times. But gluLookAt needs to be called before the gl
+            # rendering code in player, so player.update has to be split into two
+            # functions.
+            #
+            # self.this_player is an instance of Tank, but we want to call the
+            # HeadlessTank update, which excludes openGL calls
+            shapes.HeadlessTank.update(self.this_player)
+            camera_pos = np.array((self.this_player.pos[0], constants.CAMERA_HEIGHT, self.this_player.pos[2]))
+            gluLookAt(
+                # pos
+                *camera_pos,
+                # at
+                *(self.this_player.tout + camera_pos),
+                # up
+                *constants.UP
+            )
+            self.this_player.gl_update()
+
             for shape in self.groups.all_shapes:
+                if shape is self.this_player:
+                    continue
                 shape.update()
             # overlays must be updated last to render correctly
             self.lifebar.update()

bangbang/shapes.py
@@ -637,9 +637,7 @@ class Tank(HeadlessTank):
         self.client_id = client_id
         self.update_state(state)
 
-    def update(self) -> None:
-        super().update()
-
+    def gl_update(self) -> None:
         for angle, gllist in (
             (self.bangle, self.blist),
             (self.tangle, self.tlist),
@@ -651,13 +649,16 @@ class Tank(HeadlessTank):
             glCallList(gllist)
             glPopMatrix()
 
+    def update(self) -> None:
+        super().update()
+        self.gl_update()
+
     def update_state(self, state: dict) -> None:
         """Force-rewrite values such as pos, bangle, etc."""
         if "actions" in state:
             state["actions"] = set(state["actions"])
         if "pos" in state:
             state["pos"] = np.array(state["pos"])
-            print(f"old pos: {self.pos}\nnew pos: {state['pos']}\n")
         self.__dict__.update(state)
 
 